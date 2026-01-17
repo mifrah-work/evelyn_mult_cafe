4bfd292092f8f4df3f3387dd1c9587b6508ba427
dellulli <177489710+bbokari121@users.noreply.github.com>
2026-01-17 12:33:45 +0530

    Update base path to evelyn_mult_cafe

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/Multiplication_Cafe_V1/',
+  base: '/evelyn_mult_cafe/',
   server: {
     headers: {
       'Cache-Control': 'public, max-age=3600'
